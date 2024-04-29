f4ab9dced54a0b63cd04521a103b8328d51aa39a
Ali <remotework.ali@gmail.com>
2024-04-29 20:23:37 +0500

    Wrote the logic to get human choice

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function getComputerChoice(){
-    let choice = Math.random()* 3;
-    if (choice <= 1) {
+    let computerChoice = Math.random()* 3;
+    if (computerChoice <= 1) {
         console.log("rock")
-    } else if (choice <= 2) {
+    } else if (computerChoice <= 2) {
         console.log("paper")
     } else (
         console.log("scissors")
     )
-    return choice;
+    return computerChoice;
 }
 
 console.log(getComputerChoice())
+
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choice One", "");
+    if (humanChoice == 'rock' || humanChoice == 'paper' || humanChoice == 'scissors') {
+        console.log("Right!")
+    } else {
+        console.log("Please write only 'rock', 'paper', 'scissors'")
+    }
+    return humanChoice
+}
+
+console.log(getHumanChoice())
