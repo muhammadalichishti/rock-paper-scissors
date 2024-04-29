0e5b2b7c3872873387eb5da3571cd040574bf3e7
Ali <remotework.ali@gmail.com>
2024-04-29 19:42:12 +0500

    Wrote the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function getComputerChoice(rock, paper, scissors){
-    return Math.random(rock, paper, scissors)
+function getComputerChoice(){
+    let choice = Math.random()* 3;
+    if (choice <= 1) {
+        console.log("rock")
+    } else if (choice <= 2) {
+        console.log("paper")
+    } else (
+        console.log("scissors")
+    )
+    return choice;
 }
 
 console.log(getComputerChoice())
